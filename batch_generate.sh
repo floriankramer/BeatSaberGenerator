a6f65b1ebce446b2bbf828c43d96a08866eab2fc
Florian Kramer <flo.kramer@arcor.de>
2019-01-25 00:11:28 +0100

    Added parallelization and multiple inputs to the batch script

--- batch_generate.sh
+++ batch_generate.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 source ./setup_env.sh
 
-for f in $(find $1 -type f -name "*.ogg"); do
-  NAME=$(basename "$f")
-  python ./src/generate.py -i "$f" "$2/$NAME"
+# find the last arguemnt
+for OUTPUT; do true; done
+
+MAX_JOBS=$(nproc --ignore=1)
+CURRENT_JOBS=0
+
+for inp in "$@"; do
+  if ! [ "$inp" = "$OUTPUT" ] ; then 
+    for f in $(find $inp -type f -name "*.ogg"); do
+      NAME=$(basename "$f")
+      python ./src/generate.py -i "$f" "$OUTPUT/$NAME" &
+      CURRENT_JOBS=$(($CURRENT_JOBS + 1))
+      if [ $CURRENT_JOBS -eq $MAX_JOBS ] ; then
+        # wait for all currently queued processes
+        wait
+        CURRENT_JOBS=0
+      fi
+    done
+  fi
 done
+
+# wait for the remaining processes
+wait
